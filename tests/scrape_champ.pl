0dc34ecb769e83460d097ff1f60dbe384e853e2e
jmcnab <jmcnab@usinternet.com>
2023-02-24 20:52:46 -0600

    removed unecessary directories

--- tests/scrape_champ.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python 
-#
-
-import urllib2, json
-
-url = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/1.json"
-data = urllib2.urlopen(url).read()
-data = json.loads(data)
-print data
